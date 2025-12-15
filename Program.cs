206b40cd652347ea0ad542f259e212fc1e07bc10
DJEDIAILAZHAR <lazhardjed@gmail.com>
2025-12-15 09:31:31 +0100

    Initialize ASP.NET Core app with PostgreSQL support
    Set up a new ASP.NET Core application with PostgreSQL support and automatic database creation.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using QuranSchool.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// إضافة دعم لـ PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// إنشاء الجداول تلقائيًا عند التشغيل
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
