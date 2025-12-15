1872f73ca424dd4ae62f902ece8d709fbb49c41b
DJEDIAILAZHAR <lazhardjed@gmail.com>
2025-12-15 15:43:38 +0100

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,7 +5,7 @@ public class Student
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Age { get; set; }
-    public string GuardianPxone { get; set; } = string.Empty;
+    public string GuardianPhone { get; set; } = string.Empty;
     public string Level { get; set; } = "مبتدئ";
     public DateTime EnrollmentDate { get; set; } = DateTime.Now;
 }
